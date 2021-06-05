e4ea06ae378e5e7288fdaf42e03835eaa98335fc
sree121 <sreedharatrs@gmail.com>
2021-06-06 00:26:08 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -2,11 +2,19 @@ exports.func = function() {
   /// bubble sort
 
   function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
     return arr;
   }
-
+  // i j
+  // 0 0
   // Write Javascript code!
-  var result = bubbleSort([3, 2, 4, 5]);
+  var result = bubbleSort([3, 2, 4]);
   const appDiv = document.getElementById('app');
   appDiv.innerHTML = `${JSON.stringify(result)}`;
 };
