97b42b99816f19b5b3cae082c574cd5216d3b491
sree121 <sreedharatrs@gmail.com>
2021-06-06 13:25:15 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -2,8 +2,8 @@ exports.func = function() {
   /// bubble sort
 
   function bubbleSort(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      for (let j = 0; j < arr.length; j++) {
+    for (let i = arr.length; i > 0; i--) {
+      for (let j = 0; j < i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
           [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
