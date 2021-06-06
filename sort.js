da33c45c11d9d24b1236312d381b0ad5355e0f8a
sree121 <sreedharatrs@gmail.com>
2021-06-06 13:38:16 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -1,6 +1,5 @@
 exports.func = function() {
   /// bubble sort
-
   function bubbleSort(arr) {
     for (let i = arr.length; i > 0; i--) {
       for (let j = 0; j < i - 1; j++) {
@@ -11,10 +10,23 @@ exports.func = function() {
     }
     return arr;
   }
+  // var result = bubbleSort([3, 2, 4, 1, 11, 23]);
   // i j
   // 0 0
   // Write Javascript code!
-  var result = bubbleSort([3, 2, 4, 1, 11, 23]);
+
+  function selection(arr) {
+    let min = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] > arr[j + 1]) {
+          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+      }
+    }
+    return arr;
+  }
+  var result = selection([3, 2, 4, 1, 11, 23]);
   const appDiv = document.getElementById('app');
   appDiv.innerHTML = `${JSON.stringify(result)}`;
 };
