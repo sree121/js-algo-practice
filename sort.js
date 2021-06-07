ca196c31584b03867758c54d7d436127977e65c9
sree121 <sreedharatrs@gmail.com>
2021-06-07 11:49:26 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -36,20 +36,36 @@ exports.sort = function() {
   */
 
   //Insertion sort
-  function Insertion(arr) {
-    for (let i = 0; i < arr.length; i++) {
-      let low = i;
-      for (let j = i + 1; j < arr.length; j++) {
-        if (arr[low] > arr[j]) {
-          low = j;
-        }
+  // function Insertion(arr) {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     let low = i;
+  //     for (let j = i + 1; j < arr.length; j++) {
+  //       if (arr[low] > arr[j]) {
+  //         low = j;
+  //       }
+  //     }
+  //     if (i !== low) [arr[i], arr[low]] = [arr[low], arr[i]];
+  //   }
+  //   return arr;
+  // }
+
+  function merge(arr1, arr2) {
+    let i,
+      j = 0,
+      result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+      if (arr2[j] > arr1[i]) {
+        result.push(arr1[i]);
+        i++;
+      } else {
+        result.push(arr2[j]);
+        j++;
       }
-      if (i !== low) [arr[i], arr[low]] = [arr[low], arr[i]];
     }
-    return arr;
   }
 
-  var result = Insertion([3, 2, 4, 1, 11, 23]);
+  var result = merge([1, 2, 3], [5, 6, 7]);
   const appDiv = document.getElementById('app');
   appDiv.innerHTML = `${JSON.stringify(result)}`;
 };
