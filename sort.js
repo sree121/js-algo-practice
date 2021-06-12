48fb72caf15f1e5d853449766cd3fbde3869a7d9
sree121 <sreedharatrs@gmail.com>
2021-06-12 11:36:18 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -5,6 +5,7 @@ exports.sort = function() {
   }
   /// bubble sort
   function bubbleSort(arr) {
+    //not considering i as index -> i=arr.length
     for (let i = arr.length; i > 0; i--) {
       for (let j = 0; j < i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
