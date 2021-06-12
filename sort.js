d3b504cb7375ae7cf91af38d703c740583319f14
sree121 <sreedharatrs@gmail.com>
2021-06-12 11:37:19 +0530

    1 file modified

--- sort.js
+++ sort.js
@@ -9,7 +9,8 @@ exports.sort = function() {
     for (let i = arr.length; i > 0; i--) {
       for (let j = 0; j < i - 1; j++) {
         if (arr[j] > arr[j + 1]) {
-          [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+          swap(ar, j, j + 1);
+          // [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
         }
       }
     }
