58fd8848fe40ad652c16d9f8ccb6e0dab6fd72e3
sree121 <sreedharatrs@gmail.com>
2021-06-14 14:11:27 +0530

    2 files modified

--- DS/LinkedList/LinkedList.js
+++ DS/LinkedList/LinkedList.js
@@ -1,5 +1,20 @@
+import { Node } from './ListNode';
 export class SinglyLinkedList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      // this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
 }
-
-
--- DS/LinkedList/index.js
+++ DS/LinkedList/index.js
@@ -1,6 +1,8 @@
 import { SinglyLinkedList } from './LinkedList.js';
 exports.LinkedList = function() {
   const list = new SinglyLinkedList();
-
+  list.push('10');
+  list.push('10---0');
+  list.push('220');
   console.log(list);
 };
