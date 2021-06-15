e02ea767028fca46cbe884fe58df746524f81181
sree121 <sreedharatrs@gmail.com>
2021-06-15 12:36:01 +0530

    1 file modified

--- DS/LinkedList/LinkedList.js
+++ DS/LinkedList/LinkedList.js
@@ -17,4 +17,22 @@ export class SinglyLinkedList {
     this.length++;
     return this;
   }
+  pop() {
+    if (!this.head) return undefined;
+    let curr = this.head;
+    let newTail = curr;
+
+    while (curr.next) {
+      newTail = curr;
+      curr = curr.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return curr;
+  }
 }
