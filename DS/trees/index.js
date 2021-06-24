4318e5de5897c23b68e901f55fa976de322aa632
sree121 <sreedharatrs@gmail.com>
2021-06-24 21:46:27 +0530

    1 file modified

--- DS/trees/index.js
+++ DS/trees/index.js
@@ -4,6 +4,7 @@ exports.BinarySearchTree = function() {
   bst.insert(20);
   bst.insert(201);
   bst.insert(1);
+  bst.find(20);
   const res = bst;
   const appDiv = document.getElementById('app-algo');
   appDiv.innerHTML = `${JSON.stringify(res)}`;
