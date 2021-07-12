d307cf143a172770a5c5a58560e27140195a01bf
sree121 <sreedharatrs@gmail.com>
2021-07-12 16:40:58 +0530

    1 file modified

--- DS/trees/Tree.js
+++ DS/trees/Tree.js
@@ -53,13 +53,14 @@ export class BinarySearchTree {
     const data = [];
     const queue = [];
     let node = this.root;
-    queue.push(node.val);
+    queue.push(node);
     while (queue.length) {
       node = queue.shift();
       data.push(node.val);
       if (node.left) queue.push(node.left);
       if (node.right) queue.push(node.right);
     }
+    return data;
   }
 }
 class Node {
