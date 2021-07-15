1c5fc26d0933a8c7a00596119c8ecbcbbac1e1a2
sree121 <sreedharatrs@gmail.com>
2021-07-15 11:56:48 +0530

    2 files modified

--- DS/Graph/Graph.js
+++ DS/Graph/Graph.js
@@ -1,5 +1,14 @@
 export class Graph {
   constructor() {
-    this.root = null;
+    this.adjList = {};
+  }
+
+  addVertex(ver) {
+    if (!this.adjList[ver]) this.adjList[ver] = [];
+  }
+
+  addEdge(v1, v2) {
+    this.adjList[v1].push(v2);
+    this.adjList[v2].push(v1);
   }
 }
--- DS/Graph/index.js
+++ DS/Graph/index.js
@@ -1,8 +1,13 @@
 import { Graph } from './Graph.js';
 exports.Graph = function() {
-  const bst = new Graph();
+  const g = new Graph();
   // bst.find(1);
-  const res = 'bst.DFSPostO()';
+  g.addVertex('DEL');
+  g.addVertex('BEN');
+  g.addVertex('CHN');
+  g.addVertex('HYD');
+  g.addEdge('DEL', 'BEN');
+  const res = g;
   const appDiv = document.getElementById('app-algo');
   appDiv.innerHTML = `${JSON.stringify(res)}`;
 };
